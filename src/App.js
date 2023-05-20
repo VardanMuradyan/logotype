6514f2c8ffce5110a630d76f02acd857eae87a17
VARDAN <muradyan94vardan@gmail.com>
2023-05-20 13:45:17 +0400

    datalist

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.scss';
 
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
-import {listYear, productlist} from "./helpers/constant";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    useEffect(()=>{
+    const [dataList, setDataList] = useState([])
+    useEffect(() => {
         fetch('https://raw.githubusercontent.com/Karen919/new/main/data.json')
-            .then(data=>data.json())
-            .then(data1=>console.log(data1))
-    },[])
+            .then(data => data.json())
+            .then(data1 => setDataList(data1))
+    }, [])
+
+
     return (
         <>
             <Header/>
@@ -19,33 +21,31 @@ function App() {
             <div className="container">
                 <div className="product">
                     {
-                        productlist.map((item, index) => {
+                      dataList?.length>0 ?  dataList.map((item, index) => {
                             return (
                                 <div key={index} className="product_item">
                                     <div className="__item-img">
                                         <img key={item} src={item.img} alt="#"/>
                                     </div>
                                     <div className="__item-content">
-                                        <p className="content-title">{item.title}</p>
-                                        <h2 className="content-name">{item.name}</h2>
+                                        <p className="content-title">{item.tags}</p>
+                                        <h2 className="content-name">{item.title}</h2>
                                         <div className="content__texts">
-                                            {
-                                                listYear.map((item) =>
-                                                    <>
-                                                        <p>{item.nameUs}</p>
-                                                        <span/>
-                                                        <p>{item.yearBlog}</p>
-                                                        <span/>
-                                                        <p>{item.views}</p>
-                                                    </>)
-                                            }
+                                            <p>{item.autor}</p>
+                                            <span/>
+                                            <p>{item.date}</p>
+                                            <span/>
+                                            <p>{item.views}</p>
                                         </div>
                                         <p className="content-text">{item.text}</p>
                                     </div>
                                 </div>
                             )
-                        })
+                        }):<div>
+                          Loading....
+                      </div>
                     }
+
                 </div>
             </div>
         </>
